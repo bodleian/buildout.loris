058dfbfc77addf5b79f1e65a8c69dde054c9a5a3
Calvin Butcher <calvin.butcher@bodleian.ox.ac.uk>
2014-12-01 15:28:11 +0000

    added loris-http_cache_clean.sh and corrected hardcoded paths

--- conf/loris-http_cache_clean.sh
+++ conf/loris-http_cache_clean.sh
@@ -9,13 +9,15 @@
 LOG="${buildout:directory}/var/log/loris/cache_clean.log"
 
 # Check that the cache directories...
-IMG_CACHE_DIR="${buildout:directory}/var/cache/loris/img"
-IMG_LINKS_DIR="${buildout:directory}/var/cache/loris/links"
+IMG_CACHE_ROOT_DIR="${buildout:directory}/var/images"
+IMG_CACHE_DP_DIR="${buildout:directory}/var/cache/loris/img"
+IMG_CACHE_LINKS_DIR="${buildout:directory}/var/cache/loris/links"
 
 # ...is below a certain size...
-REDUCE_TO=1048576 #1 gb
+# REDUCE_TO=1048576 #1 gb
 # REDUCE_TO=1073741824 # 1 TB
 # REDUCE_TO=2147483648 # 2 TB
+REDUCE_TO=20971520 #20 gb
 
 # ...and when it is larger, start deleting files accessed more than a certain 
 # number of days ago until the cache is smaller than the configured size.
@@ -25,32 +27,44 @@ REDUCE_TO=1048576 #1 gb
 # you can afford MINUS the amount you expect the cache to grow in between 
 # executions of this script.
 
-current_usage () {
-	du -sk $IMG_CACHE_DIR | cut -f 1
+current_usage_cache_root () {
+	du -sk IMG_CACHE_ROOT_DIR | cut -f 1
+}
+
+current_usage_cache_dp () {
+	du -sk IMG_CACHE_DP_DIR | cut -f 1
 }
 
 delete_total=0
 max_age=60 # days
-usage=$(current_usage)
+usage=$(current_usage_cache_dp)+$(current_usage_cache_root)
 start_size=$usage
 run=1
 while [ $usage -gt $REDUCE_TO ] && [ $max_age -ge -1 ]; do
 	run=0
 
 	# files. loop (instead of -delete) so that we can keep count
-	for f in $(find $IMG_CACHE_DIR -type f -atime +$max_age); do
+	for f in $(find $IMG_CACHE_ROOT_DIR -name loris_cache.* -type f -atime +$max_age); do
 		rm $f
 		let delete_total+=1
 	done
 
-	# broken symlinks
-	find -L $IMG_LINKS_DIR -type l -delete
+	# files. loop (instead of -delete) so that we can keep count
+	for f in $(find $IMG_CACHE_DP_DIR -type f -atime +$max_age); do
+		rm $f
+		let delete_total+=1
+	done
 
-	# empty directories
-	find $IMG_CACHE_DIR -mindepth 1 -type d -empty -delete
+    # empty directories
+	find $IMG_CACHE_ROOT_DIR -mindepth 1 -type d -empty -delete
+	find $IMG_CACHE_DP_DIR -mindepth 1 -type d -empty -delete
+
+	# broken symlinks
+	find -L $IMG_CACHE_LINKS_DIR -type l -delete
+	find $IMG_CACHE_LINKS_DIR -mindepth 1 -type d -empty -delete
 
 	let max_age-=1
-	usage=$(current_usage)
+	usage=$(current_usage_cache_dp)+$(current_usage_cache_root)
 done
 
 echo -ne "$(date +[%c]) " >> $LOG
@@ -60,8 +74,3 @@ if [ $run == 0 ]; then
 else
 	echo "Cache at $usage kb (no deletes required)." >> $LOG
 fi
-
-
-
-
-
